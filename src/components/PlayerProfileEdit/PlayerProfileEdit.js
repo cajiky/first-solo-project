a0f069f0805f220d40182e05b9670b9d810e7053
Christian Johannes <cajiky@gmail.com>
2018-11-19 16:33:16 -0600

    adding the player profile component, branching off now

---
+++ src/components/PlayerProfileEdit/PlayerProfileEdit.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import {connect} from 'react-redux';
+
+class PlayerProfileEdit extends Component {
+
+    render(){
+
+
+
+        retrun(
+            <h1>Player Profile Edit Page</h1>
+        )
+    }
+}
